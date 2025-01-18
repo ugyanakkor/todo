1ef9583eb72db33fbaac31bc94b56caff58cd52b
tibor.kovacs <tibor.kovacs@strivacity.com>
2025-01-18 22:16:43 +0100

    feat(todo-service): use localstorage to persist data instead of a backend

--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -8,6 +8,13 @@ import { Todo } from '../interfaces/todo.interface';
 export class TodoService {
   public todos = signal<Array<Todo>>([]);
   private nextId = 1;
+  private todosKey = 'todos'; // Key for localStorage
+
+  constructor() {
+    /* localstorage at that point when i initialize the todos variable,
+    it has to be done in this constructor instead of line 9 */
+    this.todos = signal<Array<Todo>>(this.getTodosFromLocalStorage());
+  }
 
   public addTodo(description: string): void {
     const newTodo: Todo = {
@@ -16,15 +23,30 @@ export class TodoService {
       completed: false,
     };
     this.todos.update(previousTodoArray => [...previousTodoArray, newTodo]);
+    this.saveTodosToLocalStorage(this.todos());
   }
 
   public toggleTodoStatus(id: number): void {
     const todo = this.todos().find(todo => todo.id === id);
     if (todo) todo.completed = !todo.completed;
+    this.saveTodosToLocalStorage(this.todos());
   }
 
   public deleteTodo(id: number): void {
     const updatedTodos = this.todos().filter(todo => todo.id !== id);
     this.todos.set(updatedTodos);
+    this.saveTodosToLocalStorage(this.todos());
+  }
+
+  private saveTodosToLocalStorage(todos: Array<Todo>): void {
+    localStorage.setItem(this.todosKey, JSON.stringify(todos));
+  }
+
+  private getTodosFromLocalStorage(): Array<Todo> {
+    const todosJson = localStorage.getItem(this.todosKey);
+    if (todosJson) {
+      return JSON.parse(todosJson);
+    }
+    return [];
   }
 }
